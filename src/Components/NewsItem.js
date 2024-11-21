c4b60d3421885adf18c8a736048b72ab2d2e4cbc
Vishal580 <R20200530.VISHAL@cse.reva.edu.in>
2024-11-21 16:00:14 +0530

    Adding Previous & Next Buttons to populate NewsItems

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -6,11 +6,11 @@ export default class NewsItem extends Component {
     return (
       <div className='my-3'>
         <div className="card" style={{width: "18rem"}}>
-            <img src={imageUrl} className="card-img-top" alt="..."/>
+            <img src={!imageUrl?"https://assets-prd.ignimgs.com/2024/06/03/eldenring-shadow-fp-blogroll-1717445459923.jpg?width=1280":imageUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
                 <h5 className="card-title">{title}...</h5>
                 <p className="card-text">{description}...</p>
-                <a href={newsUrl} target = "_blank" rel="noopener noreferrer" className="btn btn-sm btn-primary">Read</a>
+                <a href={newsUrl} target = "_blank" rel="noopener noreferrer" className="btn btn-sm btn-dark">Read</a>
             </div>
         </div>
       </div>
