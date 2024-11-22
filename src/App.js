56f64ee12c30a4325be2a861f1c2c005b84be900
Vishal580 <R20200530.VISHAL@cse.reva.edu.in>
2024-11-22 18:29:49 +0530

    Fetching News category wise

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './Components/Navbar';
 import News from './Components/News';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Navbar/>
-        <News pageSize='6' country='us' category='technology'/>
+        <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<News key="General" pageSize="6" country="us" category="General" />}/>
+            <Route exact path="/business" element={<News key="Business" pageSize="6" country="us" category="Business" />}/>
+            <Route exact path="/technology" element={<News key="Technology" pageSize="6" country="us" category="Technology" />}/>
+            <Route exact path="/entertainment" element={<News key="Entertainment" pageSize="6" country="us" category="Entertainment" />}/>
+            <Route exact path="/health" element={<News key="Health" pageSize="6" country="us" category="Health" />}/>
+            <Route exact path="/science" element={<News key="Science" pageSize="6" country="us" category="Science" />}/>
+            <Route exact path="/sports" element={<News key="Sports" pageSize="6" country="us" category="Sports" />}/>
+          </Routes>
+        </Router>
       </div>
     )
   }
